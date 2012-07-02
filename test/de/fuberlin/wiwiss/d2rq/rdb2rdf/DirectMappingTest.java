e81e90a3b659d096e7630e69e4a0688de3c6ace5
Aftab Iqbal <aftabiq@gmail.com>
2012-07-02 16:57:33 +0100

    logic of getTestLists() method changed in order to avoid declaring static sized Arrays

--- test/de/fuberlin/wiwiss/d2rq/rdb2rdf/DirectMappingTest.java
+++ test/de/fuberlin/wiwiss/d2rq/rdb2rdf/DirectMappingTest.java
@@ -1,7 +1,7 @@
 package de.fuberlin.wiwiss.d2rq.rdb2rdf;
 
 import java.io.File;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,22 +29,24 @@ public class DirectMappingTest {
 	}
 	
 	@Parameters
-	public static Collection getTestLists() {
+	public static Collection<Object[]> getTestLists() {
+		Collection<Object[]> lstTests = new ArrayList<Object[]>();
 		File dirPath = new File(TS_PATH);
 		File[] dir = dirPath.listFiles();
-		String[][] arr = new String[dir.length][3];
-		int index=0;
 		String absolutePath = "";
+		Object[] arr;
+		
 		for (File f:dir) {
-			if (f.isDirectory()) {
+			if (f.isDirectory() && new File(f.getAbsolutePath()+ "/directGraph.ttl").exists()) {
 				absolutePath = f.getAbsolutePath();
-				arr[index][0] = "jdbc:hsqldb:mem:" + f.getName();
-				arr[index][1] = absolutePath + "/create.sql";
-				arr[index][2] = absolutePath + "/directGraph.ttl";
-				index++;
+				arr = new Object[3];
+				arr[0] = "jdbc:hsqldb:mem:" + f.getName();
+				arr[1] = absolutePath + "/create.sql";
+				arr[2] = absolutePath + "/directGraph.ttl";
+				lstTests.add(arr);
 			}
 		}
-		return Arrays.asList(arr);
+		return lstTests;
 	}
 	
 	@Test
@@ -55,15 +57,9 @@ public class DirectMappingTest {
 		loader.setStartupSQLScript(this.sqlFile);
 		loader.setSystemBaseURI(baseURI);
 		Model d2rqModel = loader.getModelD2RQ();
-		Model model = null;
-		
-		try {
-			model = getJenaModel(this.dGraphFilePath);
-			assertEquals(true,model.isIsomorphicWith(d2rqModel));
-		} finally {
-			d2rqModel.close();
-			model.close();
-		}
+		Model model = getJenaModel(this.dGraphFilePath);
+
+		assertEquals(true,model.isIsomorphicWith(d2rqModel));
 	}
 	
 	public Model getJenaModel(String inputFileName) {
