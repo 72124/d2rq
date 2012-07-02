50224d829346679967b5a68a7ef12237b3dca390
Aftab Iqbal <aftabiq@gmail.com>
2012-07-02 13:22:36 +0100

    more code refactoring.

--- test/de/fuberlin/wiwiss/d2rq/rdb2rdf/DirectMappingTest.java
+++ test/de/fuberlin/wiwiss/d2rq/rdb2rdf/DirectMappingTest.java
@@ -1,11 +1,6 @@
 package de.fuberlin.wiwiss.d2rq.rdb2rdf;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.Collection;
 import org.junit.Test;
@@ -14,65 +9,38 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.RDFWriter;
 import com.hp.hpl.jena.util.FileManager;
 import static org.junit.Assert.assertEquals;
 import de.fuberlin.wiwiss.d2rq.SystemLoader;
-import de.fuberlin.wiwiss.d2rq.parser.MapParser;
 
 @RunWith(Parameterized.class)
 public class DirectMappingTest {
 
-	static final String TS_PATH = "doc/rdb2rdf-ts/";
-	Model model1;
-	Model model2;
+	private static final String TS_PATH = "doc/rdb2rdf-ts/";
+	private String jdbcURL = "";
+	private String sqlFile = "";
+	private String dGraphFilePath = "";
+	private static final String baseURI = "http://example.com/base/";
 	
-	public DirectMappingTest(String file1, String file2) {
-		model1 = getJenaModel(file1);
-		model2 = getJenaModel(file2);
+	public DirectMappingTest(String jdbcURL, String sqlFile, String dGraphFilePath) {
+		this.jdbcURL = jdbcURL;
+		this.sqlFile = sqlFile;
+		this.dGraphFilePath = dGraphFilePath;
 	}
 	
 	@Parameters
-	public static Collection data() {
+	public static Collection getTestLists() {
 		File dirPath = new File(TS_PATH);
 		File[] dir = dirPath.listFiles();
-		String[][] arr = new String[dir.length][2];
+		String[][] arr = new String[dir.length][3];
 		int index=0;
-		String format = "TURTLE";
-		PrintStream out = null;
-		
+		String absolutePath = "";
 		for (File f:dir) {
 			if (f.isDirectory()) {
-				SystemLoader loader = new SystemLoader();
-				loader.setMappingFileOrJdbcURL("jdbc:hsqldb:mem:" + f.getName());
-				loader.setGenerateW3CDirectMapping(true);
-				loader.setStartupSQLScript(f.getAbsolutePath() + "/create.sql");
-				loader.setSystemBaseURI("http://example.com/base/");
-				Model d2rqModel = loader.getModelD2RQ();
-				
-				try {
-					File file = new File(f.getAbsolutePath() + "/directGraph-d2rq.ttl");
-					out = new PrintStream(new FileOutputStream(file));
-					loader.setSystemBaseURI(MapParser.absolutizeURI(f.toURI().toString() + "#"));
-					RDFWriter writer = d2rqModel.getWriter(format.toUpperCase());
-					if (format.equals("RDF/XML") || format.equals("RDF/XML-ABBREV")) {
-						writer.setProperty("showXmlDeclaration", "true");
-						if (loader.getResourceBaseURI() != null) {
-							writer.setProperty("xmlbase", loader.getResourceBaseURI());
-						}
-					}
-					writer.write(d2rqModel, new OutputStreamWriter(out, "utf-8"), loader.getResourceBaseURI());
-				} catch (UnsupportedEncodingException ex) {
-					throw new RuntimeException("Can't happen -- utf-8 is always supported");
-				} catch (FileNotFoundException ex) {
-					throw new RuntimeException("Error creating file.");
-				}
-				finally {
-					out.close();
-				}
-				
-				arr[index][0] = f.getAbsolutePath() + "/directGraph.ttl";
-				arr[index][1] = f.getAbsolutePath() + "/directGraph-d2rq.ttl";
+				absolutePath = f.getAbsolutePath();
+				arr[index][0] = "jdbc:hsqldb:mem:" + f.getName();
+				arr[index][1] = absolutePath + "/create.sql";
+				arr[index][2] = absolutePath + "/directGraph.ttl";
 				index++;
 			}
 		}
@@ -81,11 +49,24 @@ public class DirectMappingTest {
 	
 	@Test
 	public void directMappingValidator() {
-		assertEquals(true,model1.isIsomorphicWith(model2));
+		SystemLoader loader = new SystemLoader();
+		loader.setMappingFileOrJdbcURL(this.jdbcURL);
+		loader.setGenerateW3CDirectMapping(true);
+		loader.setStartupSQLScript(this.sqlFile);
+		loader.setSystemBaseURI(baseURI);
+		Model d2rqModel = loader.getModelD2RQ();
+		Model model = null;
+		
+		try {
+			model = getJenaModel(this.dGraphFilePath);
+			assertEquals(true,model.isIsomorphicWith(d2rqModel));
+		} finally {
+			d2rqModel.close();
+			model.close();
+		}
 	}
 	
 	public Model getJenaModel(String inputFileName) {
-		Model model = FileManager.get().readModel(ModelFactory.createDefaultModel(),inputFileName);
-		return model;
+		return FileManager.get().readModel(ModelFactory.createDefaultModel(),inputFileName);
 	}
 }
